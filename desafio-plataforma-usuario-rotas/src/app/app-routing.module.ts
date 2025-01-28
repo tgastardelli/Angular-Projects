28462a6e36f103bb97680998912a8e47ccad1af4
tgastardelli <tgastardelli@gmail.com>
2025-01-28 00:23:16 -0300

    feat: novos componentes e rotas estáticas

--- desafio-plataforma-usuario-rotas/src/app/app-routing.module.ts
+++ desafio-plataforma-usuario-rotas/src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { GeneralComponent } from './components/general/general.component';
+import { BasicComponent } from './components/general/components/basic/basic.component';
+import { ContactComponent } from './components/general/components/contact/contact.component';
+import { AddressComponent } from './components/general/components/address/address.component';
 
 const routes: Routes = [
   {
     path: '',
     component: InicioComponent
+  },
+  {
+    path: 'general',
+    component: GeneralComponent,
+    children: [
+      {
+        path: 'basic',
+        component: BasicComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+      {
+        path: 'address',
+        component: AddressComponent
+      }
+    ]
   }
 ];
 
--- desafio-plataforma-usuario-rotas/src/app/app.module.ts
+++ desafio-plataforma-usuario-rotas/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { GeneralComponent } from './components/general/general.component';
+import { BasicComponent } from './components/general/components/basic/basic.component';
+import { ContactComponent } from './components/general/components/contact/contact.component';
+import { AddressComponent } from './components/general/components/address/address.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    GeneralComponent,
+    BasicComponent,
+    ContactComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
