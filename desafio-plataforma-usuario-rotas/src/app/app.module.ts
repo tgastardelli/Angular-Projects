f710fdc23c65d9700e7461c7db85296fdfed2116
tgastardelli <tgastardelli@gmail.com>
2025-01-28 01:31:45 -0300

    fix: ajuste

--- desafio-plataforma-usuario-rotas/src/app/app.module.ts
+++ desafio-plataforma-usuario-rotas/src/app/app.module.ts
@@ -5,13 +5,6 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { GeneralComponent } from './components/general/general.component';
-import { BasicComponent } from './components/general/components/basic/basic.component';
-import { ContactComponent } from './components/general/components/contact/contact.component';
-import { AddressComponent } from './components/general/components/address/address.component';
-import { TransactionComponent } from './components/transaction/transaction.component';
-import { CreditComponent } from './components/transaction/components/credit/credit.component';
-import { DebitComponent } from './components/transaction/components/debit/debit.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { GeneralModule } from './components/general/general.module';
 
