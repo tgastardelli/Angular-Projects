743b9f971c0305717d645ce90b092a882b629c3f
tgastardelli <tgastardelli@gmail.com>
2025-03-11 00:47:53 -0300

    feat: update user

--- projeto-usuarios/projeto-usuarios-back/server.js
+++ projeto-usuarios/projeto-usuarios-back/server.js
@@ -30,11 +30,58 @@ app.post('/login', (req, res) => {
 });
 
 
-app.put('/update-user', (req, res) => {
-    const { name, email, username, password } = req.body;
+app.put('/update-user', authenticateToken, (req, res) => {
+    const tokenUsername = req.username
+    const newUserInfos = req.body;
 
-    
+    const { name, email, username, password } = newUserInfos;
 
+    if(!name || !email || !username || !password) {
+        return res.status(400).json({ message: 'All fields (name, email, username, password) are required!' });
+    }
+
+    const USER_FOUND = USER_LIST_BD.findIndex((user) => user.username === tokenUsername);
+
+    if(USER_FOUND === -1) {
+        return res.status(403).json({ message: 'User not found.' })
+    }
+
+    USER_LIST_BD[USER_FOUND] = newUserInfos
+
+    const newToken = generateTokenOnLogin(username);
+
+    res.status(200).json({ 
+        message: 'User updated sucessfuly.',
+        token: newToken
+    });
+})
+
+app.post('/create-user', authenticateToken, (req, res) => {
+    const tokenUsername = req.username
+    const newUser = req.body;
+
+    const { name, email, username, password } = newUser;
+
+    if(!name || !email || !username || !password) {
+        return res.status(400).json({ message: 'All fields (name, email, username, password) are required!' });
+    }
+
+    const USER_TOKEN_FOUND = USER_LIST_BD.findIndex((user) => user.username === tokenUsername);
+
+    if(USER_TOKEN_FOUND === -1) {
+        return res.status(403).json({ message: 'User not found.' })
+    }
+
+    const USER_FOUND = USER_LIST_BD.findIndex((user) => user.username === newUser.username);
+    const USER_ALREADY_REGISTRED = USER_FOUND !== -1;
+
+    if(USER_ALREADY_REGISTRED) {
+        return res.status(409).json({message: 'user already registered.'})
+    }
+
+    USER_LIST_BD.push(newUser);
+
+    return res.status(201).json({ message: 'user sucessfully created.' })
 })
 
 app.post('/validate-token', authenticateToken, (req, res) => {
